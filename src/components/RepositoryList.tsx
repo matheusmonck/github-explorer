833ebe78abdea89b2f5b25a1aa3ab3a74468ce27
Kalley Matheus <matheusmonck@gmail.com>
2021-12-05 18:04:59 -0300

    end

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+
+import "../styles/repositories.scss";
+
+//
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([]); //armazena a listagem dos repositórios
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1> Lista de Repositório</h1>
+
+      <ul>
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
